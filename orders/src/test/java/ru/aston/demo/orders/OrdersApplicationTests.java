032c8774c526bf7a64eaa2a3f77f5097cecdcf58
alexsavkaev <lesh1y174@gmail.com>
2024-08-01 04:03:05 +0300

    initial commit

---
+++ orders/src/test/java/ru/aston/demo/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.demo.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ suppliers/src/main/java/ru/aston/demo/suppliers/SuppliersApplication.java
@@ -0,0 +1,13 @@
+package ru.aston.demo.suppliers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuppliersApplication.class, args);
+    }
+
+}
