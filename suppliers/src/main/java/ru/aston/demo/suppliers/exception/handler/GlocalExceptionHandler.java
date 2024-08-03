67bd64aa72269abee574ef88acefe7904fe0d4df
SergeyBichan <ugidancer@gmail.com>
2024-08-03 21:41:15 +0300

    Add global exception handler

---
+++ suppliers/src/main/java/ru/aston/demo/suppliers/exception/handler/GlocalExceptionHandler.java
@@ -0,0 +1,20 @@
+package ru.aston.demo.suppliers.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.aston.demo.suppliers.exception.AppError;
+import ru.aston.demo.suppliers.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+@Slf4j
+public class GlocalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<AppError> catchResourceNotFoundException(ResourceNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), e.getMessage()),
+                HttpStatus.NOT_FOUND);
+    }
+}
