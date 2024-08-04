3c4374507239f4bfb7453d3cde6fcd0b9b4e267a
SergeyBichan <ugidancer@gmail.com>
2024-08-04 12:38:02 +0300

    Add liquibase and some data to tables

---
+++ suppliers/src/main/resources/db/changelog/changes/002-add-some-data-to-tables.sql
@@ -0,0 +1,19 @@
+INSERT INTO suppliers(supplier_name) values ('ASD');
+INSERT INTO suppliers(supplier_name) values ('ASDW');
+INSERT INTO suppliers(supplier_name) values ('SAD');
+INSERT INTO suppliers(supplier_name) values ('XCXC');
+INSERT INTO suppliers(supplier_name) values ('vxcveA');
+INSERT INTO suppliers(supplier_name) values ('[popdfs]');
+
+INSERT INTO products(product_name, supplier_id)
+VALUES ('BREAD', 1);
+INSERT INTO products(product_name, supplier_id)
+VALUES ('MILK', 2);
+INSERT INTO products(product_name, supplier_id)
+VALUES ('POTATOE', 3);
+INSERT INTO products(product_name, supplier_id)
+VALUES ('ALCOHOL', 4);
+INSERT INTO products(product_name, supplier_id)
+VALUES ('MEAT', 5);
+INSERT INTO products(product_name, supplier_id)
+VALUES ('DISHWASHER MACHINE', 6);
