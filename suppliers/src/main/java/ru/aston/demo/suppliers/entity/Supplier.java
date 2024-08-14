5fc8ed638d87f92d553d131a20a8c85811263754
ugidancer <ugidancer@gmail.com>
2024-08-14 17:01:18 +0300

    try to fix adding new Supplier

--- suppliers/src/main/java/ru/aston/demo/suppliers/entity/Supplier.java
+++ suppliers/src/main/java/ru/aston/demo/suppliers/entity/Supplier.java
@@ -1,9 +1,11 @@
 package ru.aston.demo.suppliers.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
