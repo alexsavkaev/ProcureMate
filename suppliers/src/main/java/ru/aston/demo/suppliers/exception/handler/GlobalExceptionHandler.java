bd926ba682c8a7f7f21bca0c419f679369d4a2cf
SergeyBichan <ugidancer@gmail.com>
2024-08-18 19:18:33 +0300

    refactoring GlobalExceptionHandler

--- suppliers/src/main/java/ru/aston/demo/suppliers/exception/handler/GlobalExceptionHandler.java
+++ suppliers/src/main/java/ru/aston/demo/suppliers/exception/handler/GlobalExceptionHandler.java
@@ -5,17 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import ru.aston.demo.suppliers.controller.ProductController;
 import ru.aston.demo.suppliers.exception.AppError;
 import ru.aston.demo.suppliers.exception.ResourceNotFoundException;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = RestController.class)
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<AppError> catchResourceNotFoundException(ResourceNotFoundException e) {
         log.error(e.getMessage(), e);
+
         return new ResponseEntity<>(new AppError(e.getMessage(), LocalDateTime.now()),
                 HttpStatus.NOT_FOUND);
     }
