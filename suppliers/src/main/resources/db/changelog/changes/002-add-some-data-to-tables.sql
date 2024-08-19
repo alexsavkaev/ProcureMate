e11cb7bc3fab134fa1db7f36b97ad224632c42b9
SergeyBichan <ugidancer@gmail.com>
2024-08-19 14:28:55 +0300

    add data to sql scripts

--- suppliers/src/main/resources/db/changelog/changes/002-add-some-data-to-tables.sql
+++ suppliers/src/main/resources/db/changelog/changes/002-add-some-data-to-tables.sql
@@ -5,15 +5,19 @@ INSERT INTO suppliers(supplier_name) values ('XCXC');
 INSERT INTO suppliers(supplier_name) values ('vxcveA');
 INSERT INTO suppliers(supplier_name) values ('[popdfs]');
 
-INSERT INTO products(product_name, supplier_id)
-VALUES ('BREAD', 1);
-INSERT INTO products(product_name, supplier_id)
-VALUES ('MILK', 2);
-INSERT INTO products(product_name, supplier_id)
-VALUES ('POTATOE', 3);
-INSERT INTO products(product_name, supplier_id)
-VALUES ('ALCOHOL', 4);
-INSERT INTO products(product_name, supplier_id)
-VALUES ('MEAT', 5);
-INSERT INTO products(product_name, supplier_id)
-VALUES ('DISHWASHER MACHINE', 6);
+INSERT INTO products(product_name, product_price, supplier_id)
+VALUES ('BREAD', 1000 ,1);
+INSERT INTO products(product_name, product_price, supplier_id)
+VALUES ('MILK', 1500, 2);
+INSERT INTO products(product_name,product_price, supplier_id)
+VALUES ('POTATOE', 1100, 3);
+INSERT INTO products(product_name, product_price,  supplier_id)
+VALUES ('ALCOHOL', 150, 4);
+INSERT INTO products(product_name, product_price, supplier_id)
+VALUES ('MEAT', 600, 5);
+INSERT INTO products(product_name, product_price, supplier_id)
+VALUES ('DISHWASHER MACHINE',550, 6);
+INSERT INTO products(product_name, product_price, supplier_id)
+VALUES ('WHITE BREAD', 900 ,1);
+INSERT INTO products(product_name, product_price, supplier_id)
+VALUES ('RED Wine', 2700 ,1);
