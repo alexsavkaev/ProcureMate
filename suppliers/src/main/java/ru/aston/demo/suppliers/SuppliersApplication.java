260e6fb8cd26d865138f77a1de38ecf9e36c4604
ugidancer <ugidancer@gmail.com>
2024-08-19 12:27:51 +0300

    remove empty string

--- suppliers/src/main/java/ru/aston/demo/suppliers/SuppliersApplication.java
+++ suppliers/src/main/java/ru/aston/demo/suppliers/SuppliersApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuppliersApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SuppliersApplication.class, args);
     }
