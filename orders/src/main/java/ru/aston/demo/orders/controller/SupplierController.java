64eb2b2014c0e02eed393e470025010581d574f8
alexsavkaev <lesh1y174@gmail.com>
2024-08-20 13:27:01 +0300

    fix api's for SupplierController

--- orders/src/main/java/ru/aston/demo/orders/controller/SupplierController.java
+++ orders/src/main/java/ru/aston/demo/orders/controller/SupplierController.java
@@ -4,26 +4,22 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.aston.demo.orders.dto.ProductDto;
 import ru.aston.demo.orders.service.impl.SupplierServiceImpl;
 
 @RestController
-@RequestMapping("/suppliers")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 public class SupplierController {
   private final SupplierServiceImpl supplierService;
 
-  @GetMapping("/priceList/refresh")
-  public ResponseEntity<List<ProductDto>> refreshPrices(){
+  @GetMapping("/all")
+  public ResponseEntity<List<ProductDto>> getPriceList(){
     return supplierService.getPriceList();
   }
-  @PatchMapping("/priceList")
-  public ResponseEntity<List<ProductDto>> getPrices(){
-    return supplierService.getPrices();
-  }
+
 
 }
 
